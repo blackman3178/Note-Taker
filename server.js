45725734b76c31559fbcf284d55567055caf3208
Noah Blackman <nablackm@usc.edu>
2021-10-16 14:55:59 -0400

    Added app.listen feature in server.js

--- server.js
+++ server.js
@@ -26,3 +26,7 @@ app.get("*", (req, res) =>
 app.get("/notes", (req, res) =>
     res.sendFile(path.join(__dirname, "/public/notes.html"))
 );
+
+app.listen(PORT, () => 
+    console.log(`App listening @ http://localhost:${PORT} 🚀`)
+);
