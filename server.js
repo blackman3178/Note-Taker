8b510ef31d81231591d04fc191871d1f6a8831f1
Noah Blackman <nablackm@usc.edu>
2021-10-16 14:51:05 -0400

    initial commit with routes, middleware, & helper files created. additionaly, added initial HTML routes in server.js

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const { appendFile } = require("fs");
+const path = require("path");
+const { clog } = require("./middleware/clog");
+const api = require('./routes/index');
+
+const PORT = process.env.port || 3001;
+
+const app = express();
+
+//imports cusotm middleware "clog"
+app.use(clog);
+
+// middleware for parsign JSON & urlencoded data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
+
+app.use("/api", api);
+
+app.use(express.static('public'));
+
+app.get("*", (req, res) => 
+    res.sendFile(path.join(__dirname,"/public/index.html"))
+);
+
+app.get("/notes", (req, res) =>
+    res.sendFile(path.join(__dirname, "/public/notes.html"))
+);
