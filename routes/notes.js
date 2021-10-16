b92e7b6fe224d326a16f78417ef134880d2ffe67
Noah Blackman <nablackm@usc.edu>
2021-10-16 19:02:48 -0400

    added post route in notes.js, npm installed uuid, initializing in next step.

--- routes/notes.js
+++ routes/notes.js
@@ -1,11 +1,31 @@
 const notes = require("express").Router();
 const { readAndAppend, readFromFile } = require("../helpers/fsUtils");
 
+
 // GET route for retrieving notes from the JSON db
 notes.get("/", (req, res) => {
     // why ../ here but ./ in example, doesnt make sense
-    readFromFile('../db/db.json').then((data) => 
+    readFromFile('./db/db.json').then((data) => 
     res.json(JSON.parse(data))
     );
 });
 
+notes.post("/", (req, res) => {
+
+    //destructure the note object passed in 
+    const {title, text} = req.body;
+    
+    if (req.body) {
+        const newNote = {
+            title,
+            text,
+        };
+        readAndAppend(newNote, "./db/db.json");
+        res.json("Note added successfully!🚀");
+    } else {
+        res.error("Error in adding note :/");
+    }
+
+});
+
+module.exports = notes;
